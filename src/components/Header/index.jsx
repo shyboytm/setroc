4330fb0c27c7e4a3af162d76c930ca4d714a72ee
Dennis Cortés <hi@cortes.us>
2017-02-19 11:16:47 -0600

    Adds SVG logo to Header, adds circle element and adds color transitioning to circle background element

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
 
+import Logo from '../Logo';
+
 class Header extends Component {
   render() {
-
-    const imgLogo = require('../../img/cortes-logo.png');
-
     return (
-      <div id="Header" className="w-100">
-        <div className="db dt-l w-100 border-box pv4 ph6-l">
-          <Link to="/" className="db dtc-l v-mid white link dim w-100 w-25-l tc tl-l mb2 mb0-l" title="Pokém">
-            <img className="w4" src={imgLogo} alt="Dennis Cortes Logo" />
+      <div id="Header" className="w-90 center mv4-ns mv2 ">
+        <div className="dib dt-l w-100 border-box pv4 ph6-l ph3">
+          <Link to="/" className="pt3 dib dtc-l v-mid white link dim w-40" title="Dennis Cortes">
+            <Logo />
           </Link>
-          <div className="db dtc-l v-mid w-70 center w-75-l tc tr-l">
-            <Link to="/" className="link pointer ttu tracked black-30 hover-blue fw7">About &amp; Contact</Link>
+          <div className="dib dtc-l v-mid-ns center w-60 tr-l tc">
+            <Link to="/" className="dib link pointer ttu tracked black-30 hover-blue fw7">About / Contact</Link>
           </div>
         </div>
       </div>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -8,19 +8,22 @@ class Home extends Component {
     return (
       <div id="Home">
         <Header />
-        <div className="w-60-l w-80-m w-90 center cf">
-          <h1 className="f1-ns f2 fw4 lh-copy">Dennis Cortés is a Designer, Illustrator, Developer, and Writer</h1>
-          <span className="f4 black-50 ttu tracked fw7">Available for Work <span className="green f3">•</span></span>
-          <div className="w-70-l w-100 fr-l mt4-l mt5">
+        <div id="IntroCircle" className="br-100 bg-black-05 relative o-50" />
+        <div className="w-60-l w-80-m w-90 center cf relative">
+          <div className="nt8 z-0">
+            <h1 className="f1-ns f2 fw4 lh-copy z-0">Dennis Cortés is a Designer, Illustrator, Developer, and Writer</h1>
+            <span className="f4-ns f5 black-50 ttu tracked fw7 z-0 ">Available for Work <span id="blinker" className="green f3">•</span></span>
+          </div>
+          <div className="w-70-l w-100 fr-l mt0-l mt5">
             <p className="f3-ns f4 black-50 lh-subtitle">
               Currently leading design at <a className="subtitle-link link black-50 hover-blue" href="http://satchelhealth.com">Satchel Health</a> and building
               resources and experiences at <a className="subtitle-link link black-50 hover-blue" href="http://usequarry.com">Quarry</a>. I also write a bit for
               my weekly personal blog, <a className="subtitle-link link black-50 hover-blue" href="https://medium.com/the-rate-of-change">The Rate of Change</a>.
-              Feel free email me at <a className="subtitle-link link black-50 hover-blue" href="mailto:hi@cortes.us">hi@cortes.us</a> if you'd like to get in touch.
+              Email me at <a className="subtitle-link link black-50 hover-blue" href="mailto:hi@cortes.us">hi@cortes.us</a> if you'd like to get in touch.
             </p>
           </div>
         </div>
-        <div className="w-100 bg-black-05 mt4">
+        <div className="w-100 bg-black-05 mt5">
           <span className="db">Work</span>
           <div className="pv4 w-70-l w-80-m w-90 center">
            <TitleWithDescription />
