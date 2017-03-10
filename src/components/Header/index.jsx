f6a107cfe84fcab104600789601fa8b76a3b0cd0
Dennis Cortés <hi@cortes.us>
2017-03-09 20:59:42 -0600

    Implements new info design for home page and creates components for each of the elements, also adds forward class to CSS

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,27 +7,27 @@ class Header extends Component {
   render() {
     return (
       <div id="Header" className="z-max top-0 w-100 center pv3 bg-white ph4-l ph3 cf">
-        <div className="fixed-l w-50-l w-100 fl dib pb0-l pb3">
+        <div className="fixed-l w-40-l w-100 fl dib pb0-l pb3">
           <Link to="/" className="b pv2 f6 db-l dib link black lh-title tooltip">
             Dennis Cortés <span className="green ml1 f5 tooltip">•<span className="tooltiptext green dib f6 fw4">Available for work!</span></span>
           </Link>
-          <Link to="/" className="pb2 f6 db-l dib ml0-l ml3 link black-50 hover-blue lh-title" title="About">
+          <Link to="/" className="pb2 f6 db-l ml0-l ml3 link black-50 hover-blue lh-title" title="About">
             About
           </Link>
-          <Link to="/" className="f6 db-l dib ml0-l ml3 link black-50 hover-blue lh-title" title="Blog">
+          <Link to="/" className="f6 db-l ml0-l ml3 link black-50 hover-blue lh-title" title="Blog">
             Blog
           </Link>
         </div>
         <div className="w-50-l w-100 fr pv2">
           <div className="w-50 fl">
-            <Link to="mailto:hi@cortes.us" className="f6 db pb2 link pointer hover-blue black-50">hi@cortes.us</Link>
-            <Link to="https://twitter.com/fromcortes" className="f6 db pb2 link pointer hover-light-blue black-50">Twitter</Link>
-            <Link to="https://dribbble.com/fromcortes" className="f6 db pb2 link pointer hover-pink black-50">Dribbble</Link>
+            <a href="mailto:hi@cortes.us" className="f6 db pb2 link pointer hover-blue black-50">hi@cortes.us</a>
+            <a href="https://twitter.com/fromcortes" className="f6 db pb2 link pointer hover-light-blue black-50">Twitter</a>
+            <a href="https://dribbble.com/fromcortes" className="f6 db pb2 link pointer hover-pink black-50">Dribbble</a>
           </div>
           <div className="w-50 fl">
-            <Link to="https://twitch.tv/cortesarts" className="f6 db pb2 link pointer hover-purple black-50">Twitch</Link>
-            <Link to="https://instagram.com/fromcortes" className="f6 db pb2 link pointer hover-dark-blue black-50">Instagram</Link>
-            <Link to="https://github.com/fromcortes" className="f6 db pb2 link pointer hover-gold black-50">Github</Link>
+            <a href="https://twitch.tv/cortesarts" className="f6 db pb2 link pointer hover-purple black-50">Twitch</a>
+            <a href="https://instagram.com/fromcortes" className="f6 db pb2 link pointer hover-dark-blue black-50">Instagram</a>
+            <a href="https://github.com/fromcortes" className="f6 db pb2 link pointer hover-gold black-50">Github</a>
           </div>
         </div>
       </div>
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Header from '../Header';
+import InfoSection from '../InfoSection';
 import TitleWithDescription from '../TitleWithDescription';
 import CaseStudySelection from '../CaseStudySelection';
 
@@ -17,57 +18,26 @@ class Home extends Component {
           <div className="nt8 z-0 ml5-l ml2 pl3-l">
             <h1 className="f1-ns f2 fw4 lh-copy z-0 mt0 mb3">Dennis Cortés is a Designer, Illustrator, Developer, and Writer</h1>
             <p className="f3-ns f4 black-50 lh-subtitle mt0 measure">
-              Currently leading design at <a className="subtitle-link link black-50 hover-blue" href="http://satchelhealth.com">Satchel Health</a> and building
-              resources and experiences at <a className="subtitle-link link black-50 hover-blue" href="http://usequarry.com">Quarry</a>. I also write a bit for
-              my weekly personal blog, <a className="subtitle-link link black-50 hover-blue" href="https://medium.com/the-rate-of-change">The Rate of Change</a>.
-              Email me at <a className="subtitle-link link black-50 hover-blue" href="mailto:hi@cortes.us">hi@cortes.us</a> if you'd like to get in touch.
+              Currently leading design at <a className="dotted-link link black-50 hover-blue" href="http://satchelhealth.com">Satchel Health</a> and building
+              resources and experiences at <a className="dotted-link link black-50 hover-blue" href="http://usequarry.com">Quarry</a>. I also write a bit for
+              my weekly personal blog, <a className="dotted-link link black-50 hover-blue" href="https://medium.com/the-rate-of-change">The Rate of Change</a>.
+              Email me at <a className="dotted-link link black-50 hover-blue" href="mailto:hi@cortes.us">hi@cortes.us</a> if you'd like to get in touch.
             </p>
           </div>
         </div>
         <div id="Work" className="w-100 bb bt b--black-10 mt5-ns mt2">
           <span className="dib-l dn rotated-text-l ml5 mt6">Work</span>
-          <div className="pv5 w-80-ns w-100 center">
-             <span className="dn-l db f4 ml4 pl3 mb4 rotated-text">Work</span>
-             <TitleWithDescription
-                selectionLink="https://dribbble.com/fromcortes"
-                selectionTitle="Design Portfolio"
-                selectionDescription="Most recent design work and experiments." />
-            <TitleWithDescription
-                selectionLink="http://usequarry.com"
-                selectionTitle="Quarry"
-                selectionDescription="Curated resources for humans." />
-            <TitleWithDescription
-                selectionLink="http://coffee.cortes.us"
-                selectionTitle="Taste Notes"
-                selectionDescription="Coffee thoughts and reference." />
-            <TitleWithDescription
-                selectionLink="http://sogol.co"
-                selectionTitle="Sogol"
-                selectionDescription="Hand crafted and on-demand logos for your ideas." />
-            <TitleWithDescription
-                selectionLink="http://fieldy.co"
-                selectionTitle="Fieldy"
-                selectionDescription="Unofficial resources for lovers of Field Notes." />
-            <TitleWithDescription
-                selectionLink="http://calligritype.us"
-                selectionTitle="Calligritype"
-                selectionDescription="A place for artists and designers." />
-            <TitleWithDescription
-                selectionLink="http://instagram.com/illustree"
-                selectionTitle="Illustree"
-                selectionDescription="A resource for illustration inspiration." />
-            <TitleWithDescription
-                selectionLink="http://shopdrones.co"
-                selectionTitle="Shop Drones Co."
-                selectionDescription="Drone shopping made beautifully simple." />
-            <TitleWithDescription
-                selectionLink="http://music.cortes.us"
-                selectionTitle="Cordio"
-                selectionDescription="Music by me for people to listen to." />
-            <TitleWithDescription
-                selectionLink="http://pokem.cortes.us"
-                selectionTitle="Pokém"
-                selectionDescription="A Pokémon team and dex tracker." />
+          <span className="dn-l db f4 ml4 pl3 mb4 rotated-text">Work</span>
+          <div className="content-end flex flex-wrap pv5 w-80-ns w-100 center cf">
+             <InfoSection
+                infoSectionTitle="Work"
+                infoSectionDescription="We design, architect and build creative digital solutions with the highest attention to aesthetic, functional and content detail to ensure a flawless and truly engaging user experience." />
+            <InfoSection
+               infoSectionTitle="Recognition"
+               infoSectionDescription="We design, architect and build creative digital solutions with the highest attention to aesthetic, functional and content detail to ensure a flawless and truly engaging user experience." />
+            <InfoSection
+               infoSectionTitle="Writing"
+               infoSectionDescription="We design, architect and build creative digital solutions with the highest attention to aesthetic, functional and content detail to ensure a flawless and truly engaging user experience." />
           </div>
         </div>
         <div id="CaseStudies" className="bb b--black-10 w-100">
