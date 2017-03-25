ac8b3d8ab90d212282cd1662115dabba9a5b3fb4
Dennis Cortés <hi@cortes.us>
2017-03-25 15:03:32 -0500

    Adds correct titles and emojis to Approach section

--- src/components/Pages/About/index.jsx
+++ src/components/Pages/About/index.jsx
@@ -56,45 +56,45 @@ class About extends Component {
             <div className="content-end flex flex-wrap pv3 w-100 cf white">
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blank"
-                  toolTipText="😍"
+                  title="Inclusion"
+                  toolTipText="👥"
                   paragraph="My family and my friends are constant reminders of humility. My girlfriend and my
                              dogs keep me level headed and inspired to balance my work and my life. Creating
                              a better, more enjoyable life for others motivates my work." />
               </div>
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blankity"
-                  toolTipText="🤓"
+                  title="Accessibility"
+                  toolTipText="🎨"
                   paragraph="When I'm not designing or writing code you can find me writing, playing video games,
                              learning about coffee, trying out new tech, treating my dogs like kids, catching Pokémon, or making music." />
               </div>
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blank"
-                  toolTipText="🤓"
+                  title="Simplicity"
+                  toolTipText="⚫️"
                   paragraph="When I'm not designing or closing elements you can find me writing, playing video games,
                              learning about coffee, catching Pokémon, or making sick beatz." />
               </div>
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blank"
-                  toolTipText="😍"
+                  title="Discovery"
+                  toolTipText="🌎"
                   paragraph="My family and my friends are constant reminders of humility. My girlfriend and my
                              dogs keep me level headed and inspired to balance my work and my life. Creating
                              a better, more enjoyable life for others motivates my work." />
               </div>
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blankity"
-                  toolTipText="🤓"
+                  title="Hierarchy"
+                  toolTipText="🔠"
                   paragraph="When I'm not designing or writing code you can find me writing, playing video games,
                              learning about coffee, trying out new tech, treating my dogs like kids, catching Pokémon, or making music." />
               </div>
               <div className="w-33-l w-50-m w-100 pr5-ns items-start">
                 <TitleParagraph
-                  title="Blank"
-                  toolTipText="🤓"
+                  title="Ethics"
+                  toolTipText="❤️"
                   paragraph="When I'm not designing or closing elements you can find me writing, playing video games,
                              learning about coffee, catching Pokémon, or making sick beatz." />
               </div>
