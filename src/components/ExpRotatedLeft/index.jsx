17f377da01af78ff30e77566e93a231c982af474
Dennis Cortés <hi@cortes.us>
2017-03-26 16:41:27 -0500

    Removes negative margins on experience section so items are full-width

--- src/components/ExpRotatedLeft/index.jsx
+++ src/components/ExpRotatedLeft/index.jsx
@@ -6,7 +6,7 @@ class ExpRotatedRight extends Component {
     const { year } = this.props;
 
     return (
-      <div id="ExpRotatedRight" className="items-end nr4-l w-20-l w-33-m w-50 dib ph4-l ph3 pv3-l pv2 bg-white-l rotate--10 shadow-large-l mb3">
+      <div id="ExpRotatedRight" className="items-end w-20-l w-33-m w-50 dib ph4-l ph3 pv3-l pv2 bg-white-l rotate--10 shadow-large-l mb3">
          <h2 className="f5 b center mw6 black-40">{year}</h2>
          <ul className="list pl0 ml0 center lh-copy">
            {this.props.children}
