e87abd81e3c94393ce92a3ca8fb89c7a7669385f
Dennis Cortés <hi@cortes.us>
2017-04-08 16:19:42 -0500

    Adds LazyLoad to project and secret page

--- src/components/Pages/Secret/SecretImage/index.jsx
+++ src/components/Pages/Secret/SecretImage/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LazyLoad from 'react-lazy-load';
 
 class SecretImage extends Component {
   render() {
@@ -7,7 +8,9 @@ class SecretImage extends Component {
 
     return (
       <div className="item">
-        <img alt="Secret" src={imageSource} />
+        <LazyLoad offset={1000}>
+        <img className="w-100" alt="Secret" src={imageSource} />
+        </LazyLoad>
       </div>
     );
   }
