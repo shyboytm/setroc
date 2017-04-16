dcd5feeb5e82c94ef7984ae5690b7de768b12254
Dennis Cortés <hi@cortes.us>
2017-04-16 10:59:15 -0500

    Improves Livestream page layout and information

--- src/components/Pages/Livestream/index.jsx
+++ src/components/Pages/Livestream/index.jsx
@@ -9,7 +9,23 @@ class Livestream extends Component {
       <div id="Livestream" className="h-100 bg-black white">
         <Header />
         <div className="w-100 center pt5-l pv3 cf">
-          <div className="w-70-l w-100 h0 fl relative pb-45">
+          <div className="db-l dn w-100 center pv3 bg-black ph4-l ph3 cf">
+            <div className="w-60-l w-100 fl dib pv2 mb0-l mb2">
+              <p className="white-70 lh-copy mv1-l pt1-l mv0">I stream my work and sometimes gaming here T, W, and Th at 7:30PM CST.</p>
+            </div>
+            <div className="w-40-l w-100 fl dib pv2 mt1">
+              <a className="f6 link dim br2 ph3 pv2 dib white bg-purple fr-l fn ttu b tracked tc" href="https://twitch.tv/cortesarts">Watch on Twitch</a>
+            </div>
+          </div>
+          <div className="dn-l db w-90 center mb5">
+            <p className="f4 lh-subtitle white mb4 tc">
+              On mobile? I recommend watching from the Twitch app for a better experience
+            </p>
+            <div className="w-50-l w-100 tc dib">
+              <a className="f6 link dim br2 ph3 pv2 dib white bg-purple fr-l fn ttu b tracked tc" href="https://twitch.tv/cortesarts">Watch on Twitch</a>
+            </div>
+          </div>
+          <div className="db-l dn w-70-l w-100 h0 fl relative pb-45">
             <iframe
               className="absolute w-100 h-100 top-0 left-0"
               src="https://player.twitch.tv/?channel=cortesarts"
@@ -18,7 +34,7 @@ class Livestream extends Component {
               scrolling="no">
             </iframe>
           </div>
-          <div className="w-30-l w-100 fr-l fl h0 relative pb-45">
+          <div className="db-l dn w-30-l w-100 fr-l fl h0 relative pb-45">
             <iframe
               className="absolute w-100 h-100 top-0 left-0 fr-l fl"
               frameorder="0"
