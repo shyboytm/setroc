cb1138fa88ae56bba38c8d28ff9d9897cae2ef2e
Dennis Cortés <hi@cortes.us>
2017-05-26 17:13:44 -0500

    Updates About page copy and blog posts on home page

--- src/components/Pages/Home/HomeInfo/index.jsx
+++ src/components/Pages/Home/HomeInfo/index.jsx
@@ -97,6 +97,12 @@ class HomeInfo extends Component {
             </InlineList>
             <InlineList>
               <InlineListItem
+                inlineListItemLink="http://blog.cortes.us/design-and-develop-smaller"
+                inlineListItemTitle="Design and Develop Smaller" />
+              <InlineListItem
+                inlineListItemLink="http://blog.cortes.us/best-thing-learned-as-adult"
+                inlineListItemTitle="The Best Thing I Learned as an Adult" />
+              <InlineListItem
                 inlineListItemLink="http://blog.cortes.us/stop-charging-hourly"
                 inlineListItemTitle="Stop Charging by the Hour" />
                <InlineListItem
@@ -105,12 +111,6 @@ class HomeInfo extends Component {
               <InlineListItem
                 inlineListItemLink="http://blog.cortes.us/do-you-need-degree-design"
                 inlineListItemTitle="Do You Need a Degree in Design?" />
-              <InlineListItem
-                inlineListItemLink="http://blog.cortes.us/disadvantages-of-working-remote"
-                inlineListItemTitle="Disadvantages of Working Remote" />
-              <InlineListItem
-                inlineListItemLink="http://blog.cortes.us/advantages-of-working-remote"
-                inlineListItemTitle="Advantages of Working Remote" />
             </InlineList>
           </div>
           <div className="w-33-l w-50-m w-80 center pr5-ns items-start">
