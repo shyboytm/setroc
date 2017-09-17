7880f3b015069b85468944bda1255d683e15839c
fromcortes <hi@cortes.us>
2017-09-17 13:17:12 -0500

    Adds EveryDay carry section and Items, also adds a Section component and implements it into Tools pag

--- src/components/Pages/Tools/index.jsx
+++ src/components/Pages/Tools/index.jsx
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 import Footer from '../../Footer';
 import Item from '../../Item';
+import Section from '../../Section';
 import Title from '../../Title';
 
 class Books extends Component {
   render() {
 
+    // Setup
     const macBookPro      = require('../../../img/tools/setup/macbook-pro-13-inch.jpg');
     const roostStand      = require('../../../img/tools/setup/roost-laptop-stand.jpg');
     const magicKeyboard   = require('../../../img/tools/setup/apple-magic-keyboard.jpg');
@@ -20,20 +22,35 @@ class Books extends Component {
     const rissla          = require('../../../img/tools/setup/rissla-ikea-mat.jpg');
     const bekant          = require('../../../img/tools/setup/bekant-ikea-desk.jpg');
 
+    // Every Day Carry
+    const iPhone7         = require('../../../img/tools/setup/apple-iphone-7-plus.jpg');
+    const totalleeCase    = require('../../../img/tools/setup/totallee-thin-iphone-case.jpg');
+    const sonyA5100       = require('../../../img/tools/setup/sony-a5100-camera.jpg');
+    const appleWatch      = require('../../../img/tools/setup/apple-watch-series-1.jpg');
+    const satechiReader   = require('../../../img/tools/setup/satechi-usb-sd-card.jpg');
+    const incaseBackpack  = require('../../../img/tools/setup/incase-backpack.jpg');
+    const gucciWallet     = require('../../../img/tools/setup/gucci-signature-wallet.jpg');
+    const simpleCard      = require('../../../img/tools/setup/simple-bank-card.jpg');
+    const finalCard       = require('../../../img/tools/setup/final-bank-card.jpg');
+    const vanguard        = require('../../../img/tools/setup/baron-fig-vanguard.jpg');
+    const fieldNotes      = require('../../../img/tools/setup/field-notes-notebooks.jpg');
+
     return (
       <div id="Tools" className="animated fadeIn black">
 
         <div className="w-75-ns w-100 center mv5-ns mv4 ph0-ns ph3 z-0 tc">
           <h1 className="f2-ns f3 fw4 lh-title leitura">Tools</h1>
-          <p className="fw4 lh-copy center measure black-60">
+          <p className="fw4 lh-copy center measure black-60 mb4">
             These are the tools I use on a daily basis for work and life.
-            Everything from apps I use to games I play. Enjoy!
+            Everything from apps I use, stuff I always carry, to games I play.
+            Affliliate links are used when possible to help support my free to use
+            projects. Enjoy!
           </p>
           <Title title="Last Updated Sep. 28th" />
           {/* Shortcuts */}
           <ul className="list ph0 pv4-ns pv3">
             <li className="dib mr3">
-              <a href="#Setup" target="_blank" rel="nofollow" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
+              <a href="#Setup" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
                 <svg className="mr2" xmlns="http://www.w3.org/2000/svg" fill="rgba(0,0,0,1.0)" width="20" height="20" viewBox="0 0 28 18">
                   <title>Laptop Custom Icon</title>
                   <path d="M27,22V8a2,2,0,0,0-2-2H7A2,2,0,0,0,5,8V22H2v1a2,2,0,0,0,2,2H28a2,2,0,0,0,2-2V22ZM8,9H24V20H8Z" transform="translate(-2 -6)"/>
@@ -42,7 +59,7 @@ class Books extends Component {
               </a>
             </li>
             <li className="dib mr3">
-              <a href="#Setup" target="_blank" rel="nofollow" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
+              <a href="#EveryDay" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
                 <svg className="mr2" xmlns="http://www.w3.org/2000/svg" fill="rgba(0,0,0,1.0)" width="18" height="18" viewBox="0 0 16 26">
                   <title>Backpack Custom Icon</title>
                   <path d="M19,6.59V6a3,3,0,0,0-6,0v.59A8,8,0,0,0,8,14V27a2,2,0,0,0,2,2H22a2,2,0,0,0,2-2V14A8,8,0,0,0,19,6.59ZM14,6a2,2,0,0,1,4,0v.26a7.76,7.76,0,0,0-4,0Zm-1,6a1,1,0,0,1,1-1h4a1,1,0,0,1,1,1v1a1,1,0,0,1-1,1H14a1,1,0,0,1-1-1Zm9,11H20v2a1,1,0,0,1-2,0V23H10V21H22Z" transform="translate(-8 -3)"/>
@@ -51,7 +68,7 @@ class Books extends Component {
               </a>
             </li>
             <li className="dib mr3">
-              <a href="#Setup" target="_blank" rel="nofollow" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
+              <a href="#Apps" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
                 <svg className="mr2" xmlns="http://www.w3.org/2000/svg" fill="rgba(0,0,0,1.0)" width="18" height="18" viewBox="0 0 28 28">
                   <title>App Web Custom Icon</title>
                   <path d="M22,16a6,6,0,1,1-6-6A6,6,0,0,1,22,16Zm7.12-2.12a3,3,0,1,0,0,4.24A3,3,0,0,0,29.12,13.88ZM2.88,18.12a3,3,0,1,0,0-4.24A3,3,0,0,0,2.88,18.12ZM21.59,7.59a2,2,0,1,0,2.83,0A2,2,0,0,0,21.59,7.59Zm-14,14a2,2,0,1,0,2.83,0A2,2,0,0,0,7.59,21.59Zm16.83,0a2,2,0,1,0,0,2.83A2,2,0,0,0,24.41,21.59Zm-14-14a2,2,0,1,0,0,2.83A2,2,0,0,0,10.41,7.59Zm3.46-4.71a3,3,0,1,0,4.24,0A3,3,0,0,0,13.88,2.88Zm4.24,26.24a3,3,0,1,0-4.24,0A3,3,0,0,0,18.12,29.12Z" transform="translate(-2 -2)"/>
@@ -60,7 +77,7 @@ class Books extends Component {
               </a>
             </li>
             <li className="dib mr3">
-              <a href="#Setup" target="_blank" rel="nofollow" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
+              <a href="#Gaming" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
                 <svg className="mr2" xmlns="http://www.w3.org/2000/svg" fill="rgba(0,0,0,1.0)" width="18" height="18" viewBox="0 0 26.7 25">
                   <title>Gaming Custom Icon</title>
                   <path d="M29.3,23l-1.89-11.5a3.39,3.39,0,0,0-2.73-2.74h0a48.53,48.53,0,0,0-17.36,0h0a3.39,3.39,0,0,0-2.73,2.74L2.7,23A3.39,3.39,0,0,0,9,25.29l1.38-2.93a3,3,0,0,1,2.13-1.66h0a17.71,17.71,0,0,1,7,0,3,3,0,0,1,2.11,1.63l1.43,3A3.39,3.39,0,0,0,29.3,23ZM13,14.5a.5.5,0,0,1-.5.5h-1a.5.5,0,0,0-.5.5v1a.5.5,0,0,1-.5.5h-1a.5.5,0,0,1-.5-.5v-1a.5.5,0,0,0-.5-.5h-1a.5.5,0,0,1-.5-.5v-1a.5.5,0,0,1,.5-.5h1a.5.5,0,0,0,.5-.5v-1a.5.5,0,0,1,.5-.5h1a.5.5,0,0,1,.5.5v1a.5.5,0,0,0,.5.5h1a.5.5,0,0,1,.5.5Zm7.5-.5A1.5,1.5,0,1,1,22,12.5,1.5,1.5,0,0,1,20.5,14Zm4,4A1.5,1.5,0,1,1,26,16.5,1.5,1.5,0,0,1,24.5,18Zm-8-11h-1a.5.5,0,0,1-.5-.5V3a1,1,0,0,1,1-1h0a1,1,0,0,1,1,1V6.5A.5.5,0,0,1,16.5,7Z" transform="translate(-2.65 -2)"/>
@@ -69,7 +86,7 @@ class Books extends Component {
               </a>
             </li>
             <li className="dib mr3">
-              <a href="#Setup" target="_blank" rel="nofollow" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
+              <a href="#Coffee" className="items-center flex f6 fw4 mb3 br-pill db pv2 ph3 link black bg-black-10 hover-bg-black-20">
                 <svg className="mr2" xmlns="http://www.w3.org/2000/svg" fill="rgba(0,0,0,1.0)" width="18" height="18" viewBox="0 0 28 26">
                   <title>Coffee Custom Icon</title>
                   <path d="M30,26a2,2,0,0,1-2,2H4a2,2,0,0,1-2-2V25H30ZM28,8a1,1,0,0,1,1,1v3a6,6,0,0,1-5,5.91V18a5,5,0,0,1-5,5H11a5,5,0,0,1-5-5V9A1,1,0,0,1,7,8Zm-1,2H24v5.86A4,4,0,0,0,27,12ZM13,6.5V3a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V6.5a.5.5,0,0,0,.5.5h1A.5.5,0,0,0,13,6.5Zm6,0V4a1,1,0,0,0-1-1h0a1,1,0,0,0-1,1V6.5a.5.5,0,0,0,.5.5h1A.5.5,0,0,0,19,6.5Z" transform="translate(-2 -2)"/>
@@ -82,22 +99,11 @@ class Books extends Component {
 
         <div className="w-60-l w-90 center mv5">
             {/* Setup */}
-            <div id="Setup" className="tl-ns tc bb b--black-10 pb3 mb4 flex flex-row-ns flex-column items-center justify-between">
-              <div>
-                {/* Title */}
-                <h2 className="f3-ns f4 fw4 lh-title leitura mt0 mb2">
-                  Setup
-                </h2>
-                {/* Description */}
-                <p className="f6 fw4 lh-copy measure-narrow black-60">
-                  Gear and furniture I use on the go and at my home office
-                </p>
-              </div>
-              {/* Item Number */}
-              <span className="lh-copy f2-ns f4 fw6 mb0-ns mb3">
-                12
-              </span>
-            </div>
+            <Section
+              id="Setup"
+              title="Setup"
+              description="Gear and furniture I use on the go and at my home office"
+              number="12" />
             <div className="content-end flex flex-wrap">
               <Item
                 href="https://www.apple.com/macbook-pro"
@@ -159,6 +165,70 @@ class Books extends Component {
                 image={bekant}
                 title="Bekant Desk"
                 description="I really need a standing desk" />
+            </div>
+
+            {/* Every Day Carry */}
+            <Section
+              id="EveryDay"
+              title="Every Day Carry"
+              description="Things I carry with me every day from tech to random items"
+              number="11" />
+            <div className="content-end flex flex-wrap">
+              <Item
+                href="https://www.apple.com/macbook-pro"
+                image={iPhone7}
+                title="iPhone 7 Plus"
+                description="Matte Black 32GB" />
+              <Item
+                href="http://amzn.to/2wyw6Hy"
+                image={totalleeCase}
+                title="Totallee Thin Case"
+                description="Naked iPhone without the scratching" />
+              <Item
+                href="http://amzn.to/2xI1pof"
+                image={sonyA5100}
+                title="Sony α5100 Camera"
+                description="Great quality pictures, pocket-sized" />
+              <Item
+                href="http://amzn.to/2wm8DxN"
+                image={appleWatch}
+                title="Apple Watch Series 1"
+                description="Have worn it every day since its release" />
+              <Item
+                href="http://amzn.to/2jBqDOZ"
+                image={satechiReader}
+                title="Satechi USB-C SD Card Reader"
+                description="For editing pictures on-the-go with that dongle life" />
+              <Item
+                href="http://amzn.to/2xI63CH"
+                image={incaseBackpack}
+                title="Incase Laptop Backpack"
+                description="Durable, all-purpose backpack I've had for 3 years" />
+              <Item
+                href="https://www.gucci.com/us/en/pr/gifts/monogramming/gucci-signature-wallet-p-365466CWC1R1000?position=10&listName=SearchResultGridComponent"
+                image={gucciWallet}
+                title="Gucci Signature Wallet"
+                description="Gift from Italy that I've used the past 5 years" />
+              <Item
+                href="https://simple.com/friends/FPCJH2Y"
+                image={simpleCard}
+                title="Simple Card"
+                description="Best banking experience I've ever had" />
+              <Item
+                href="https://signup.getfinal.com/?invite=IKB0P97ZYL"
+                image={finalCard}
+                title="Final Credit Card"
+                description="Reliable, safe, and useful for building credit" />
+              <Item
+                href="https://www.baronfig.com/products/shopvanguard"
+                image={vanguard}
+                title="Baron Fig Notebooks"
+                description="I use dot-grid charcoal plus size Vanguards" />
+              <Item
+                href="http://amzn.to/2haGY8V"
+                image={fieldNotes}
+                title="Field Notes Pocket Notebooks"
+                description="For on-the-go notes and sketches, I use kraft or pitch black ones" />
             </div>
 
         </div>
---
+++ src/components/Section/index.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Section extends Component {
+  render() {
+
+    const {
+      id,
+      title,
+      description,
+      number
+    } = this.props;
+
+    return (
+      <div id={id} className="tl-ns tc bb b--black-10 pb3 mb4 flex flex-row-ns flex-column items-center justify-between mt5">
+        <div>
+          <h2 className="f3-ns f4 fw4 lh-title leitura mt0 mb2">
+            {title}
+          </h2>
+          <p className="f6 fw4 lh-copy measure-narrow black-60">
+            {description}
+          </p>
+        </div>
+        <span className="lh-copy f2-ns f4 fw6 mb0-ns mb3">
+          {number}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Section;
