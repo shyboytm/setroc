474fe5bd69338f783df3daf46d1c2562fc587ca3
fromcortes <hi@cortes.us>
2018-01-13 15:53:50 -0600

    Starts reverting to master

--- src/components/Pages/Home/index.jsx
+++ src/components/Pages/Home/index.jsx
@@ -10,42 +10,10 @@ class Home extends Component {
     const aboutImage = require("../../../img/about-picture.jpg");
 
     return (
-      <div id="Home" className="black bg-black-90 f6 pv5 ph3">
-        {/*
+      <div id="Home" className="black bg-white lh-copy f6">
           <Desktop />
           <Mobile />
           <Footer />
-        */}
-        <div className="animated fadeIn center w-40-l w-60-l w-90">
-          <div className="cf mb0">
-            <p className="bg-white-30 br-chat-left dib fl fw3 lh-copy mb0 ph3 pv2 white">
-              I'm new here, who's this? What do you do?
-            </p>
-          </div>
-          <span className="db ttu tracked mt1 mb3 f7 white-30">You</span>
-          <div className="cf">
-            <p className="bg-white-90 black-80 br-chat-right dib fr lh-copy ph3 pv2 mv1 mw5">
-              Hello! I'm Dennis Cortés 👋
-            </p>
-          </div>
-          <div className="cf">
-            <div className="bg-about-image bg-top bg-white-90 black-80 br-chat-right-image cover dib fr h5 lh-copy mt1 mw5 w-100">
-            </div>
-          </div>
-          <div className="cf">
-            <p className="bg-white-80 black-80 br-chat-right-media dib fr fw5 lh-copy ph4 pv3 mw5 mb1 mt0 w-100">
-              Dennis Cortes <br />
-              <span className="black-40 fw4 f7">Email: hi@cortes.us</span>
-            </p>
-          </div>
-          <div className="cf">
-            <p className="bg-white-90 black-80 br-chat-right-middle dib fr lh-copy pa3 mv1 mw5">
-              I’m a digital designer and illustrator that codes based in Memphis, TN from
-              Bayamón, Puerto Rico. I love working on projects that require multidisciplinary
-              skill sets solving complex issues that make the lives of people better.
-            </p>
-          </div>
-        </div>
       </div>
     );
   }
