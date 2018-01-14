7dbe9244ac7a1fe65b3c5046032383a9245461c1
fromcortes <hi@cortes.us>
2018-01-13 18:34:44 -0600

    Updates Item font styling

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -16,7 +16,7 @@ class Item extends Component {
           <a href={href} target="_blank" rel="nofollow" className="db-ns flex justify-around items-center link pointer black hover-black-30 no-outline">
             <img src={image} alt="Item or Product" className="grow no-drag w-100-ns w-30 ml0-ns ml2 bb-ns bn b--black-10 br2 br--top-ns" />
             <div className="db-ns flex flex-column justify-between pa3">
-              <h1 className="leitura fw4 f4-ns f5 mt0 mb1-ns mb0 lh-title">{title}</h1>
+              <h1 className="leitura fw5 f5 mt0 mb1-ns mb0 lh-title">{title}</h1>
               <p className="f6 black-60 mt0 mb3-ns mb1" style={{lineHeight: "1.35rem"}}>
                 {description}
               </p>
