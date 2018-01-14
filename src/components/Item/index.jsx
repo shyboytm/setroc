d7c9c2283939b6fbf19dfbc78d0ef29189696437
fromcortes <hi@cortes.us>
2018-01-13 21:18:35 -0600

    Improves accessibility of Item image

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -14,7 +14,7 @@ class Item extends Component {
       <div className="items-start relative ph3 pv3 w-25-l w-50-m w-100">
         <div className="db-ns flex bg-white overflow-hidden ba b--black-10 shadow-small br2">
           <a href={href} target="_blank" rel="nofollow" className="db-ns flex justify-around items-center link pointer black hover-black-30 no-outline">
-            <img src={image} alt="Item or Product" className="grow no-drag w-100-ns w-30 ml0-ns ml2 bb-ns bn b--black-10 br2 br--top-ns" />
+            <img src={image} alt={title} className="grow no-drag w-100-ns w-30 ml0-ns ml2 bb-ns bn b--black-10 br2 br--top-ns" />
             <div className="db-ns flex flex-column justify-between pa3">
               <h1 className="leitura fw5 f5 mt0 mb1-ns mb0 lh-title">{title}</h1>
               <p className="f6 black-60 mt0 mb3-ns mb1" style={{lineHeight: "1.35rem"}}>
