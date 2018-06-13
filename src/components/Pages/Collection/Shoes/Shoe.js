733e5338d992bc786eb47b33604e4c2a6a7ed218
fromcortes <hi@cortes.us>
2018-06-12 22:21:29 -0500

    Adds more shoes to collection page

--- src/components/Pages/Collection/Shoes/Shoe.js
+++ src/components/Pages/Collection/Shoes/Shoe.js
@@ -7,20 +7,20 @@ class Shoe extends React.Component {
 
     return (
       <li className="pa2 w-33-l w-50-m w-100">
-        <a className="link overflow-hidden pointer shadow-small up"
+        <a className="link overflow-hidden pointer"
           href={details.href}
           target="_blank" rel="nofollow"
           title={`Check out ${details.name}`}>
-          <div style={{backgroundColor: `${details.color}`}}>
+          <div style={{backgroundColor: `${details.color}`}} className="br3 overflow-hidden shadow-small up">
             <img src={`http://cortes.us/images/collection/shoes/${details.image}.jpg`}
                  alt={`${details.name} ${details.model}`}
-                 className="multiply hover-unmultiply bg-animate nb2"/>
-            <div className="pa3 tc">
+                 className="bg-animate nb2"/>
+            <div className="pa3">
               <h2 className="black-60 f3 fw8 mv0">{details.name}</h2>
               <h3 className="black-40 mt0 mb3">{details.model}</h3>
               <div className="flex items-center justify-between w-100">
-                <span className="black-40 f6 fw6 ttu">Got {details.date}</span>
-                <span className="black-40 fw6 mv0">${details.price}</span>
+                <span className="black-40 f6 fw6">Got {details.date}</span>
+                <span className="black-40 fw6 mv0"><span className="f6">Paid</span> ${details.price}</span>
               </div>
             </div>
           </div>
--- src/components/Pages/Collection/Shoes/shoes.js
+++ src/components/Pages/Collection/Shoes/shoes.js
@@ -8,23 +8,50 @@ const shoes = {
     name: 'Light Bones',
     price: '175'
   },
+  airJordan1ShadowFlyknit: {
+    color: '#B1B6BA',
+    date: '05/25/18',
+    href: 'https://www.twelvesouth.com/store/product/airfly',
+    image: 'air-jordan-1-shadow-flyknit',
+    model: 'Air Jordan 1',
+    name: 'Shadow Flyknits',
+    price: '95'
+  },
   airJordan1HomeageToHome: {
-    color: '#FFB0B4',
-    date: '05/17/18',
+    color: '#E64455',
+    date: '05/19/18',
     href: 'https://www.twelvesouth.com/store/product/airfly',
     image: 'air-jordan-1-homage-to-home',
     model: 'Air Jordan 1',
     name: 'Homage to Homes',
-    price: '138'
+    price: '160'
   },
   airJordan1CyberMonday: {
-    color: '#E2E2E2',
+    color: '#878684',
     date: '05/17/18',
     href: 'https://www.twelvesouth.com/store/product/airfly',
     image: 'air-jordan-1-cyber-monday',
     model: 'Air Jordan 1',
     name: 'Cyber Mondays',
     price: '138'
+  },
+  adidasProphereGreyHeather: {
+    color: '#F48146',
+    date: '05/17/18',
+    href: 'https://www.twelvesouth.com/store/product/airfly',
+    image: 'adidas-prophere-grey-heather',
+    model: 'Adidas Prophere',
+    name: 'Grey Heathers',
+    price: '75'
+  },
+  adidasEQTSupportCoreBlacks: {
+    color: '#C8CAC9',
+    date: '02/21/18',
+    href: 'https://www.twelvesouth.com/store/product/airfly',
+    image: 'adidas-eqt-support-core-black',
+    model: 'Adidas EQT Support',
+    name: 'Core Blacks',
+    price: '89'
   },
 };
 
