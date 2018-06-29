7f7f5d8efaa8c20ed1827b098a7c94a0a16bba99
fromcortes <hi@cortes.us>
2018-06-28 22:12:33 -0500

    Adds HM-03 and starts changing music page for this release

--- src/components/Pages/Music/index.jsx
+++ src/components/Pages/Music/index.jsx
@@ -13,6 +13,7 @@ class Mentoring extends Component {
     const pageName = "Music";
     const pageDescription = "I'm also a beat-maker and producer working under the alias Cordio; mixing elements of ambient, electronic, jazz, lo-fi, and hip-hop music genres";
 
+    const hm03 = require("../../../img/music/hm-03-vinyl.png");
     const hm02 = require("../../../img/music/hm-02-vinyl.png");
     const hm01 = require("../../../img/music/hm-01-vinyl.png");
 
@@ -37,11 +38,11 @@ class Mentoring extends Component {
           <meta property="og:image" content="https://www.cortes.us/images/hm-01-by-cordio.png"/>
         </Helmet>
 
-        <div>
-          <div className="bg-black-20 overflow-hidden pt5 pb6 ph4 relative tc-ns tl z-0">
+        <div className="relative">
+          <div className="bg-black-10 bg-cordio cover overflow-hidden pt5 pb6 ph4 tc-ns tl z-0">
             <div className="center w-80-l w-90">
               <h1 className="blur-late f1 ttu fw8 flex items-center justify-center-ns">
-                <svg id="CordioLogo" className="mr3 spin-full shadow-large" data-name="Cordio" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 401.04 401.04" height="35" width="35"><title>Cordio Logo</title><path d="M258.21,258.21c-78.31,78.31-78.31,205.27,0,283.58s205.27,78.31,283.58,0,78.31-205.27,0-283.58S336.52,179.9,258.21,258.21ZM506.34,506.34,293.66,293.66A150.39,150.39,0,0,1,506.34,506.34Z" transform="translate(-199.48 -199.48)"/></svg>
+                <svg id="CordioLogo" className="mr3 spin-full shadow-large" data-name="Cordio" xmlns="http://www.w3.org/2000/svg" fill="#000000" viewBox="0 0 401.04 401.04" height="35" width="35"><title>Cordio Logo</title><path d="M258.21,258.21c-78.31,78.31-78.31,205.27,0,283.58s205.27,78.31,283.58,0,78.31-205.27,0-283.58S336.52,179.9,258.21,258.21ZM506.34,506.34,293.66,293.66A150.39,150.39,0,0,1,506.34,506.34Z" transform="translate(-199.48 -199.48)"/></svg>
                 Cordio
               </h1>
               <p className="center fw5 f4 lh-subtitle measure black-60 mt0 mb4" style={{letterSpacing: 0.8 + "px"}}>
@@ -49,6 +50,9 @@ class Mentoring extends Component {
               </p>
             </div>
             <div className="db">
+              <Feature
+                href="https://cordio.bandcamp.com"
+                name="Bandcamp" />
               <Feature
                 href="https://twitter.com/cordiofm"
                 name="Twitter" />
@@ -68,7 +72,7 @@ class Mentoring extends Component {
                 href="https://www.last.fm/user/fromcortes"
                 name="Last.fm" />
             </div>
-            <div className="absolute bottom--1 right-0">
+            <div className="fixed bottom--1 right-0">
               <svg version="1.1" id="CordioVinylBackground" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
               	 viewBox="0 0 477.1 483.6" width="200" height="200" fill="rgba(0,0,0,0.1)">
               <path d="M263.6,483.6h-5c0-58.4,22.7-113.2,64-154.5c41.3-41.3,96.1-64,154.5-64v5c-57,0-110.6,22.2-151,62.5
@@ -118,6 +122,42 @@ class Mentoring extends Component {
               <path d="M230.1,483.6h-2c0-66.5,25.9-129,72.9-176.1c47-47,109.6-72.9,176.1-72.9v2c-66,0-128,25.7-174.7,72.3
               	S230.1,417.6,230.1,483.6z"/>
               </svg>
+            </div>
+          </div>
+
+          <div className="bg-white bb b--black-10 pv5 overflow-hidden w-100">
+            <div className="center w-75-ns w-90">
+              <img src={hm03} className="animated fadeInUp fl-l w-60-l w-100" />
+              <div className="fl-l center w-40-l w-100">
+                <h3 className="animated blur-late f1-l f2 mr3 mb3 black">
+                  HM-03
+                </h3>
+                <span className="db fw6 f6 mb3 ttu tracked-tiny black-60">Jul 05 2018 · 6 track EP · 16 min</span>
+                <h4 className="bg-text-clip fw6 shine black">Places to Listen to HM-03</h4>
+                <div className="center flex flex-wrap w-100">
+                  <a href="https://goo.gl/1MoEx6" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/spotify/626262/30' alt='Listen on Spotify'  />
+                  </a>
+                  <a href="https://goo.gl/G4jUsc" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/apple/626262/30' alt='Listen on Apple Music'  />
+                  </a>
+                  <a href="https://soundcloud.com/cordio/tracks" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/soundcloud/626262/30' alt='Listen on Soundcloud'  />
+                  </a>
+                  <a href="" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/play_circle_filled/626262/30' alt='Listen on YouTube'  />
+                  </a>
+                  <a href="https://goo.gl/xqy1J4" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/googleplay/626262/30' alt='Listen on Google Play'  />
+                  </a>
+                  <a href="https://goo.gl/TeKNaw" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/bandcamp/626262/30' alt='View on Bandcamp'  />
+                  </a>
+                  <a href="https://amzn.to/2qOKtYh" target="_blank" rel="nofollow" className="ba bg-animate b--black-10 br2 flex hover-bg-black-10 items-center justify-center link mb2 mr2 pa3 black">
+                    <img src='https://icon.now.sh/amazon/626262/30' alt='Listen on Amazon'  />
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
 
