3625688ba57324e230aab65f2250e1bcf5326e63
Dennis Cortes <cortes@Denniss-Air.attlocal.net>
2020-02-09 19:49:33 -0800

    Compresses images

--- src/components/H.js
+++ src/components/H.js
@@ -25,27 +25,27 @@ class Header extends Component {
 
     var classes = ({
       // Defaults
-      'black-70 lh-copy': true,
+      'black-70 lh-copy ls-fix': true,
     });
 
     if (size === 1 ) {
-      var classes = ({'black-70 f4 fw7 mb0': true});
+      var classes = ({'black-70 f4 fw7 ls-fix mb0': true});
     }
 
     if (size === 2 ) {
-      var classes = ({'black-70 f3 fw6 db': true});
+      var classes = ({'black-70 f3 fw6 db ls-fix': true});
     }
 
     if (size === 3 ) {
-      var classes = ({'black-70 f4 fw6': true});
+      var classes = ({'black-70 f4 fw6 ls-fix': true});
     }
 
     if (size === 5 ) {
-      var classes = ({'black-40 fw6 mb0 tracked ttu': true});
+      var classes = ({'black-40 fw6 ls-fix mb0 tracked ttu': true});
     }
 
     if (size === 6 ) {
-      var classes = ({'black-70 f5 fw6 mt0 mb2': true});
+      var classes = ({'black-70 f5 fw6 ls-fix mt0 mb2': true});
     }
 
     this.headerTag = chooseHeaderTag(size);
